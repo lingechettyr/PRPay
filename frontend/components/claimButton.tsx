60206a959adcf14984e6f8e313be203410a35a57
Kanishk-K <kanishkkacholia@gmail.com>
2026-01-15 14:16:04 -0500

    claim logic

--- frontend/components/claimButton.tsx
+++ frontend/components/claimButton.tsx
@@ -2,13 +2,48 @@
 import { HandCoins } from "lucide-react";
 import { Button } from "./ui/button";
 import { useWallet } from "./walletProvider";
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase/client";
 
 export default function ClaimButton({ pr_id, payout }: { pr_id: number, payout: number }) {
+    const [githubId, setGithubId] = useState<string | null>(null);
+    const [claimed, setClaimed] = useState(false);
     const { wallet } = useWallet();
+    useEffect(() => {
+        async function getUser(){
+            const {data: { user }} = await supabase.auth.getUser();
+            const githubId = user?.user_metadata?.provider_id || null;
+            setGithubId(githubId);
+        }
+        getUser();
+    }, []);
+
+    const handleClaim = async () => {
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/claimPr`,
+            {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                user_id: githubId,
+                pr_id: pr_id
+            })
+            }
+        );
+
+        const data = await res.json();
+        console.log('Claim response:', data);
+        if(data.success){
+            setClaimed(true);
+        }
+    }
+
     return (
-    <Button variant="outline" className="hover:cursor-pointer" disabled={!wallet}>
+    <Button variant="outline" className="hover:cursor-pointer" disabled={!wallet || !githubId || claimed} onClick={handleClaim}>
         <HandCoins />
-        Redeem ${payout.toFixed(2)}
+        {!claimed ? `Redeem ${payout.toFixed(2)}` : "Claimed"}
     </Button>
     )
 }
